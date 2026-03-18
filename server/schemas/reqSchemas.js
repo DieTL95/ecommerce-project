390ac49fd67ddbb5fadb7ea12cdd3cca23c8621d
Teja L <teja.louta@gmaill.com>
2026-03-18 09:10:05 +0200

    Clean up and final fix attempt

--- server/schemas/reqSchemas.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.paramSchema = void 0;
-var zod_1 = require("zod");
-exports.paramSchema = zod_1.default.object({
-    params: zod_1.default.object({
-        id: zod_1.default.uuidv4("Invalid parameter ID."),
-    }),
-});
